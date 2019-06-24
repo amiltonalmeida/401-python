6d4c2ea15b880d5f466063363b9ac33e7d76195e
Amilton <amiltonfelipedc@gmail.com>
2019-06-24 15:59:58 -0300

    Exercícicos com Python I

---
+++ python.py
@@ -0,0 +1,43 @@
+# n1 = int(input("Digite seu primeiro número: \n"))
+# n2 = int(input("Digite seu segundo número: \n"))
+# n3 = int(input("Digite seu terceiro número: \n"))
+# n4 = int(input("Digite seu quarto número: \n"))
+
+# media =str((n1+n2+n3+n4)/4)
+
+# print("Sua média foi " + media)
+
+# palavra = str(input("Digite uma frase ou palavra: \n"))
+# letra = str(input("Digite qual letra você quer pesquisar: \n"))
+
+# vezes = int(palavra.count(letra))
+
+# print("Sua letra {} aparece {} vezes na frase".format(letra, vezes))
+
+# soma = 0
+
+# i = int(input("Quantas vezes você quer somar: \n"))
+# x = i
+
+# for i in range(i):
+#     n = int(input("Digite um número \n"))
+#     soma += n
+
+# media = soma/x
+
+
+# print ("A média dos números digitados é {}".format(media))
+
+
+x1 = int(input("Digite o primeiro número: \n"))
+x2 = int(input("Digite o segundo número: \n"))
+
+soma = x1+x2
+subtracao = x1-x2
+multiplicacao =x1*x2
+divisao =x1/x2
+
+print("A soma dos números digitados é {}".format(soma))
+print("A subtração dos números digitados é {}".format(subtracao))
+print("A multiplicação dos números digitados é {}".format(multiplicacao))
+print("A divisão dos números digitados é {}".format(divisao))
